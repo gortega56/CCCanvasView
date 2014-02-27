89d398dc014d6ce7930096f8e6dd567403831e7c
Gabriel Ortega <gabriel@fieldlens.com>
2014-02-26 19:57:59 -0500

    Edited pin image

---
+++ TiledScrollView/CCStroke.h
@@ -0,0 +1,13 @@
+//
+//  CCStroke.h
+//  CCCanvasSample
+//
+//  Created by Gabriel Ortega on 2/26/14.
+//  Copyright (c) 2014 Clique City. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCStroke : NSObject
+
+@end
---
+++ TiledScrollView/CCStroke.m
@@ -0,0 +1,13 @@
+//
+//  CCStroke.m
+//  CCCanvasSample
+//
+//  Created by Gabriel Ortega on 2/26/14.
+//  Copyright (c) 2014 Clique City. All rights reserved.
+//
+
+#import "CCStroke.h"
+
+@implementation CCStroke
+
+@end
